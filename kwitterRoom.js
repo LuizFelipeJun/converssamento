15fa2fdc80841e63f9b76eeba18da2162638adb9
LuizFelipeJun <104800410+LuizFelipeJun@users.noreply.github.com>
2022-11-18 19:53:17 -0300

    Add files via upload

---
+++ kwitterRoom.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+   apiKey: "AIzaSyCaZ62jLT4HFygJcSMEKzMH6rC_RS3d3ew",
+   authDomain: "kwitter-insano-2.firebaseapp.com",
+   databaseURL: "https://kwitter-insano-2-default-rtdb.firebaseio.com",
+   projectId: "kwitter-insano-2",
+   storageBucket: "kwitter-insano-2.appspot.com",
+   messagingSenderId: "378623351061",
+   appId: "1:378623351061:web:5f21ff29aba6e38379f11c"
+ };
+ 
+firebase.initializeApp(firebaseConfig);
+
