be89805163e5779b27e6320965ddb9cbe9d0418a
LuizFelipeJun <104800410+LuizFelipeJun@users.noreply.github.com>
2022-12-02 20:03:01 -0300

    Add files via upload

--- kwitterRoom.js
+++ kwitterRoom.js
@@ -1,12 +1,45 @@
 const firebaseConfig = {
-   apiKey: "AIzaSyCaZ62jLT4HFygJcSMEKzMH6rC_RS3d3ew",
-   authDomain: "kwitter-insano-2.firebaseapp.com",
-   databaseURL: "https://kwitter-insano-2-default-rtdb.firebaseio.com",
-   projectId: "kwitter-insano-2",
-   storageBucket: "kwitter-insano-2.appspot.com",
-   messagingSenderId: "378623351061",
-   appId: "1:378623351061:web:5f21ff29aba6e38379f11c"
- };
+  apiKey: "AIzaSyCaZ62jLT4HFygJcSMEKzMH6rC_RS3d3ew",
+  authDomain: "kwitter-insano-2.firebaseapp.com",
+  databaseURL: "https://kwitter-insano-2-default-rtdb.firebaseio.com",
+  projectId: "kwitter-insano-2",
+  storageBucket: "kwitter-insano-2.appspot.com",
+  messagingSenderId: "378623351061",
+  appId: "1:378623351061:web:5f21ff29aba6e38379f11c"
+};
  
 firebase.initializeApp(firebaseConfig);
 
+var nomeDoUsuario = localStorage.getItem("nomeUsuario");
+document.getElementById("nomeUsuario").innerHTML = "Bem-vindo(a) " + nomeDoUsuario + "!";
+
+function addSala() {
+  var nomeDaSala = document.getElementById("roomName").value;
+  firebase.database().ref("/").child(nomeDaSala).update({
+    purpose: "adicionando nome da sala"
+  });
+  localStorage.setItem("name", nomeDaSala);
+  window.location = "kwitterPage.html"
+}
+
+function getDados() {
+  firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";
+  snapshot.forEach(function(childSnapshot) {childKey = childSnapshot.key;
+  roomNames = childKey;
+  var linha = "<div class = 'room_name' id = " + roomNames + " onclick = 'redirecionarParaONomeDaSala(this.id)'> #" + roomNames + "</div> <hr>";
+  document.getElementById("salas").innerHTML += linha;
+  });});
+}
+
+getDados();
+
+function redirecionarParaONomeDaSala(nomeDaSala) {
+  localStorage.setItem("nomeDaSala", nomeDaSala);
+  window.location = "kwitterPage.html";
+}
+
+function logout_button() {
+  localStorage.removeItem("nomeUsuario");
+  localStorage.removeItem("nomeDasala");
+  window.location = "index.html";
+}
